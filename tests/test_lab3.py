43957fedae6a50f3097ad07fa4537fe1031f36b3
Gonzalo Osco Hernadez <goh.acm@gmail.com>
2026-01-02 23:46:12 -0400

    testing

--- tests/test_lab3.py
+++ tests/test_lab3.py
@@ -69,7 +69,8 @@ def test_pipeline_processes_images_and_is_idempotent() -> None:
 
     head = wait_for_s3_key(bucket, metadata_key)
     data = read_json_from_s3(bucket, metadata_key)
-
+    print head
+    print data
     assert data["source_bucket"] == bucket
     assert data["source_key"] == TEST_IMAGE_KEY
     assert isinstance(data["width"], int) and data["width"] > 0
